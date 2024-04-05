8c965014430e5fb1dce3ab0ab16ed0012ada5d88
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-06 07:54:13 +0800

    Update exsists.php

--- exsists.php
+++ exsists.php
@@ -1,6 +1,13 @@
 <?php
 $fileName = $_POST["fileName"];
-
+if (strlen($fileName)>=1000){
+    echo "Badhacker!!!";
+    die();
+}
+if (preg_match("/php:/i", $fileName) || preg_match("/file:/i", $fileName) || preg_match("/zip:/i", $fileName)) {
+    echo "Badhacker!!!";
+    die();
+} 
 if (empty($fileName)) {
     echo "Please provide a file name.";
 } else {
